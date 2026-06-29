009431a7cd8627f3588a6490621a00429f86c345
Avishkar jadhav <avishkarjadhav24@gmail.com>
2026-06-29 16:55:50 +0530

    Few changes commited

---
+++ DSA/Overview.py
@@ -0,0 +1,129 @@
+'''
+# Coding Roadmap for Data Science / Analytics / Finance Placements
+
+## Top 10 Concepts (Priority Order)
+1. Binary Search on Answer
+2. Greedy + Mathematical Observation
+3. Hash Maps & Frequency Counting
+4. Sorting + Custom Comparators
+5. Prefix Sum & Difference Arrays
+6. Sliding Window & Two Pointers
+7. Heap (Priority Queue)
+8. BFS/DFS + Basic Graphs
+9. Dynamic Programming (Basic)
+10. Union Find (Disjoint Set)
+
+---
+
+# Top 50 Problems
+
+## Arrays
+1. Two Sum
+2. Best Time to Buy and Sell Stock
+3. Majority Element
+4. Product of Array Except Self
+5. Rotate Array
+6. Merge Intervals
+
+## Binary Search
+7. Binary Search
+8. Search Insert Position
+9. First Bad Version
+10. Koko Eating Bananas
+11. Capacity to Ship Packages Within D Days
+12. Minimum Number of Days to Make m Bouquets
+
+## Greedy
+13. Assign Cookies
+14. Lemonade Change
+15. Gas Station
+16. Jump Game
+17. Non-overlapping Intervals
+
+## Mathematical Observation
+18. Difference of Two Squares
+19. Happy Number
+20. Power of Two
+21. Integer Break
+22. Count Odd Numbers in an Interval Range
+
+## Hashing
+23. Contains Duplicate
+24. Valid Anagram
+25. Group Anagrams
+26. Top K Frequent Elements
+27. Longest Consecutive Sequence
+
+## Prefix Sum
+28. Running Sum of 1D Array
+29. Range Sum Query
+30. Subarray Sum Equals K
+31. Continuous Subarray Sum
+32. Find Pivot Index
+
+## Sliding Window
+33. Maximum Average Subarray I
+34. Longest Substring Without Repeating Characters
+35. Minimum Size Subarray Sum
+36. Permutation in String
+37. Fruit Into Baskets
+
+## Heap (Priority Queue)
+38. Kth Largest Element in an Array
+39. Top K Frequent Words
+40. Last Stone Weight
+41. Merge K Sorted Lists
+
+## Graph
+42. Number of Islands
+43. Flood Fill
+44. Clone Graph
+45. Course Schedule
+46. Rotten Oranges
+
+## Dynamic Programming
+47. Climbing Stairs
+48. House Robber
+49. Coin Change
+50. Longest Increasing Subsequence
+
+---
+
+# Bonus 20 (Very Common in Banking, Analytics & Consulting)
+
+1. Meeting Rooms
+2. Meeting Rooms II
+3. Minimum Platforms
+4. Employee Free Time
+5. Insert Interval
+6. Merge Intervals (Advanced)
+7. Job Scheduling
+8. Activity Selection
+9. Fractional Knapsack
+10. Stock Span
+11. Next Greater Element
+12. Daily Temperatures
+13. Maximum Subarray (Kadane)
+14. Maximum Product Subarray
+15. Prefix XOR
+16. Median from Data Stream
+17. LRU Cache
+18. Binary Search on Answer (Advanced)
+19. BFS in Grid Problems
+20. Simulation Problems
+
+---
+
+# Resources
+
+• LeetCode (Primary)
+• NeetCode 150
+• Codeforces Div2 A/B/C
+• AtCoder Beginner Contest
+• CSES Problem Set
+• HackerEarth OA Practice
+• Unstop Previous Coding Questions
+
+Target: Master 70–100 problems deeply instead of solving 500+ random questions.
+
+'''
